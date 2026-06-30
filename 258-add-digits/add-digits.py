7485ff30535d4eee99fe9c635df0ab8519eb337d
Aarya Makthala <aaryamakthala@gmail.com>
2026-06-30 22:52:36 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (89.17%) - LeetSync

---
+++ 258-add-digits/add-digits.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def addDigits(self, num):
+        while num>=10:
+            s=0
+            while num > 0:
+                s = s + num%10
+                num=num//10
+            num = s
+        return num
