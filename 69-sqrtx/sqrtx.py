9f27328a4eaa2a8718b7ebf77035889f8ba1416f
Aarya Makthala <aaryamakthala@gmail.com>
2026-07-12 20:12:51 +0530

    Time: 4 ms (42.34%) | Memory: 12.5 MB (17.65%) - LeetSync

--- 69-sqrtx/sqrtx.py
+++ 69-sqrtx/sqrtx.py
@@ -1,9 +1,6 @@
 class Solution(object):
     def mySqrt(self, x):
-        """
-        :type x: int
-        :rtype: int
-        """
-        x=int(x**0.5)
-        return x
-        
+        r = x
+        while r * r > x:
+            r = (r + x // r) // 2
+        return r
